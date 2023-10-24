c55c0a918b61299308606483e9d9de4508a199df
MOXHARTZ <moxhaforeva@gmail.com>
2023-10-24 14:16:36 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,8 @@ games { 'gta5' }
 author 'https://github.com/MOXHARTZ'
 client_script 'client.lua'
 server_script 'server.lua'
+repository 'https://github.com/MOXHARTZ/mx-audioplayer'
+version '1.0'
 lua54 'yes'
 
 ui_page 'ui/build/index.html'
