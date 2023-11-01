5ad404db918b5065fd7ef63721a650620fefd97a
MOXHARTZ <moxhaforeva@gmail.com>
2023-11-02 01:48:02 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -17,14 +17,14 @@ RegisterNetEvent('mx-audioplayer:play', function(url, soundId, volume, invokingR
     if customId then id = id .. ':' .. customId end
     local currentSound = checkCustomIdExist(customId)
     if currentSound then
-        exports['mx-surround']:Destroy(-1, sounds[currentSound])
+        Surround:Destroy(-1, sounds[currentSound])
         sounds[currentSound] = nil
     end
     local player = GetPlayerPed(src)
     local playerCoords = GetEntityCoords(player)
-    exports['mx-surround']:Play(-1, soundId, url, playerCoords, false, volume)
+    Surround:Play(-1, soundId, url, playerCoords, false, volume)
     if not soundId then return print('Failed to play sound') end
-    exports['mx-surround']:setDestroyOnFinish(-1, soundId, false)
+    Surround:setDestroyOnFinish(-1, soundId, false)
     sounds[id] = soundId
 end)
 
@@ -44,28 +44,28 @@ AddEventHandler('playerDropped', function()
         id = tonumber(id)
         src = tonumber(src)
         if id == src then
-            exports['mx-surround']:Destroy(-1, v)
+            Surround:Destroy(-1, v)
             sounds[k] = nil
         end
     end
 end)
 
 RegisterNetEvent('mx-audioplayer:setVolume', function(soundId, volume)
-    exports['mx-surround']:setVolumeMax(-1, soundId, volume)
+    Surround:setVolumeMax(-1, soundId, volume)
 end)
 
 RegisterNetEvent('mx-audioplayer:seek', function(soundId, position)
-    exports['mx-surround']:setTimeStamp(-1, soundId, position)
+    Surround:setTimeStamp(-1, soundId, position)
 end)
 
 RegisterNetEvent('mx-audioplayer:resume', function(soundId)
-    exports['mx-surround']:Resume(-1, soundId)
+    Surround:Resume(-1, soundId)
 end)
 
 RegisterNetEvent('mx-audioplayer:stop', function(soundId)
-    exports['mx-surround']:Stop(-1, soundId)
+    Surround:Stop(-1, soundId)
 end)
 
 RegisterNetEvent('mx-audioplayer:destroy', function(soundId)
-    exports['mx-surround']:Destroy(-1, soundId)
+    Surround:Destroy(-1, soundId)
 end)
