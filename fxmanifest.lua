efb83fc3dbf642bd37c1aa5781649571a0f96b1d
MOXHARTZ <moxhaforeva@gmail.com>
2023-12-17 01:52:13 +0300

    bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'gta5' }
 author 'https://github.com/MOXHARTZ'
 repository 'https://github.com/MOXHARTZ/mx-audioplayer'
-version '1.1.5'
+version '1.1.6'
 lua54 'yes'
 
 client_scripts {
