5bdf4424abf43ebe87abd78aa3c6bcf3e9ed3dd7
MOXHARTZ <moxhaforeva@gmail.com>
2024-04-09 23:30:22 +0300

    feat(es locale)

--- shared/config.lua
+++ shared/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Debug = true
-Config.Locale = 'en' -- 'en', 'fr', 'de', 'bg'
+Config.Locale = 'en' -- 'en', 'fr', 'de', 'bg', 'es'
 
 Config.Radio = {}
 Config.Radio.Enable = true
